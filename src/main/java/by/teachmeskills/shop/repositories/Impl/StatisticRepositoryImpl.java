2e99716f8e3f0f6e703d75e1e23305e595e156bc
AlhukSergey <alshuk73@gmail.com>
2023-08-16 21:50:09 +0300

    used JPA.

--- src/main/java/by/teachmeskills/shop/repositories/Impl/StatisticRepositoryImpl.java
+++ src/main/java/by/teachmeskills/shop/repositories/Impl/StatisticRepositoryImpl.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -21,27 +20,23 @@ public class StatisticRepositoryImpl extends StatisticRepository {
 
     @Override
     public Statistic create(Statistic entity) {
-        Session session = entityManager.unwrap(Session.class);
-        session.persist(entity);
+        entityManager.persist(entity);
         return entity;
     }
 
     @Override
     public List<Statistic> read() {
-        Session session = entityManager.unwrap(Session.class);
-        return session.createQuery("select s from S s ", Statistic.class).list();
+        return entityManager.createQuery("select s from S s ", Statistic.class).getResultList();
     }
 
     @Override
     public Statistic update(Statistic entity) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.merge(entity);
+        return entityManager.merge(entity);
     }
 
     @Override
     public void delete(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Statistic statistic = session.get(Statistic.class, id);
-        session.remove(statistic);
+        Statistic statistic = entityManager.find(Statistic.class, id);
+        entityManager.remove(statistic);
     }
 }
